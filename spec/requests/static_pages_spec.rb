83e806370f945ff798f68ffa030522d58d099c97
Trent Stegink <trent_stegink@taylor.edu>
2012-09-22 18:37:39 -0400

    After 5.2 Complete

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 describe "Static pages" do
 	let(:base_title){"Ruby on Rails Tutorial Sample App"}
 	describe "Home page" do
-		it "should have the h1 'Sample App;" do
+		it "should have the h1 'Sample App'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text =>"#{base_title} | Home")
+			page.should have_selector('title', :text => "#{base_title} | Home")
 		end
 	end
 
