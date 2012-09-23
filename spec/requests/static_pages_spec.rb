a21d6010e8ba20e706a5c81c68eb1abb57eb5614
Trent Stegink <trent_stegink@taylor.edu>
2012-09-23 14:11:07 -0400

    Section 6 Exercise 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 describe "Static pages" do
 
+
+
 	subject {page}
 	shared_examples_for "all static pages" do
 		it { should have_selector('h1', text: heading)}
@@ -45,5 +47,22 @@ describe "Static pages" do
 		it_should_behave_like "all static pages"
 		it {should have_selector 'title', text: '| Contact'}
 	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		page.should have_selector 'title', text: full_title('About Us')
+		click_link "Help"
+		page.should have_selector 'title', text: full_title('Help')
+		click_link "Contact"
+		page.should have_selector 'title', text: full_title('Contact')
+		click_link "Home"
+		click_link "Sign up now!"
+		page.should have_selector 'title', text: full_title("Sign up")
+		click_link "sample app"
+		page.should have_selector 'title', text: full_title("")
+	end
+
+
 
 end
