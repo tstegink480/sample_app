21e4f46f9e949938193029660507e70ebbd0c664
Trent Stegink <trent_stegink@taylor.edu>
2012-10-20 20:40:27 -0400

    Exercise 4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -46,7 +46,13 @@ describe "signup" do
 	it { should have_content('error') }
       end
       
+      describe "after saving the user" do
+        before { click_button submit }
+	let(:user) { User.find_by_email('user@example.com') }
 
+	it { should have_selector('title', text: user.name) }
+	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
 
 
       it "should create a user" do
